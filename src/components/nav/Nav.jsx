f44f744cd09f2f77efd1f2ea323752543a024577
alban misimi <alban.misimi.20@gmail.com>
2025-12-02 01:56:18 +0100

    made the nav bar for the protofolio still work to do

--- src/components/nav/Nav.jsx
+++ src/components/nav/Nav.jsx
@@ -1,7 +1,32 @@
 import "./Nav.css";
 
 function Nav() {
-  return <>hello</>;
+  return (
+    <>
+      <link rel="preconnect" href="https://fonts.googleapis.com"></link>
+      <link
+        rel="preconnect"
+        href="https://fonts.gstatic.com"
+        crossorigin></link>
+      <link
+        href="https://fonts.googleapis.com/css2?family=Ubuntu+Mono:wght@400;700&display=swap"
+        rel="stylesheet"></link>
+      <nav>
+        <h2 className="logo">Alban Misimi</h2>
+        <ul>
+          <li className="nav-links">
+            <a href="#">Projects</a>
+          </li>
+          <li className="nav-links">
+            <a href="#">About</a>
+          </li>
+          <li className="nav-links">
+            <a href="#">Contatcs</a>
+          </li>
+        </ul>
+      </nav>
+    </>
+  );
 }
 
 export default Nav;
